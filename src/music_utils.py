36bc4eec12e7f3a67c090908357c823f73a6a988
Victor Hugo Rangel <victorlimasg@outlook.com>
2023-08-21 11:10:25 -0300

    criei o gerador de acordes, mas tenho que automatiza-lo

--- src/music_utils.py
+++ src/music_utils.py
@@ -2,6 +2,8 @@
 
 from pydub import AudioSegment
 
+all_notes = ('A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab')
+
 class Note:
     def __init__(self, file_path):
         self.sound = AudioSegment.from_wav(file_path)
@@ -18,11 +20,25 @@ class Chord:
         for note in self.notes[1:]:
             combined_sound = combined_sound.overlay(note.get_sound())
         return combined_sound
-    
-c_note = Note("../data/notes/C3.wav")
-e_note = Note("../data/notes/E3.wav")
-g_note = Note("../data/notes/G3.wav")
 
-c_major_chord = Chord([c_note, e_note, g_note])
-chord_sound = c_major_chord.create_chord()
-chord_sound.export("../data/chords/chord_c_major.wav", format="wav")
+
+# c3_note = Note("data/notes/C3.wav")
+# g3_note = Note("data/notes/G3.wav")
+# c4_note = Note("data/notes/C4.wav")
+# e4_note = Note("data/notes/E4.wav")
+# g4_note = Note("data/notes/G4.wav")
+
+
+
+
+
+
+# if __name__ == '__main__':
+
+#     c_major_chord = Chord([c3_note, g3_note, c4_note, e4_note, g4_note])
+    
+#     chord_sound = c_major_chord.create_chord()
+#     chord_sound.export("data/chords/chord_c_major.wav", format="wav")
+
+for index, notes in enumerate(all_notes,0):
+    print(notes, all_notes[index+4], all_notes[index+4+3])
