182a59ef4864e1e8c253d4b2cf7b9d1951603ede
Victor Hugo Rangel <victorlimasg@outlook.com>
2023-08-22 09:20:58 -0300

    implementando a automação da criação dos acordes, ainda falta muito para ficar funcional

--- src/music_utils.py
+++ src/music_utils.py
@@ -2,8 +2,6 @@
 
 from pydub import AudioSegment
 
-all_notes = ('A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab')
-
 class Note:
     def __init__(self, file_path):
         self.sound = AudioSegment.from_wav(file_path)
@@ -21,6 +19,45 @@ class Chord:
             combined_sound = combined_sound.overlay(note.get_sound())
         return combined_sound
 
+def create_note_instances(base_note, octave_range):
+    notes = []
+    for octave in octave_range:
+        note_instance = None
+        note_file = f"data/notes/{base_note}{octave}.wav"
+        
+        try:
+            note_instance = Note(note_file)
+        except FileNotFoundError:
+            print(f'{note_file} nao encontrado') 
+            pass
+        notes.append(note_instance)
+    return notes
+
+def create_chord_instance(base_note, chord_type, modifier, octave_range):
+    chord_name = f"chord_{base_note}{octave_range[0]}_{chord_type}_{modifier}.wav"
+    chord_notes = create_note_instances(base_note, octave_range)
+    chord = Chord(chord_notes)
+    chord_sound = chord.create_chord()
+    chord_sound.export(f"data/chords/{chord_name}", format="wav")
+
+# all_notes = ('A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab')
+# octaves_chords = (('0','1'),('1','2'),('2','3'),('3','4'),('4','5'),('5','6'),('6','7'))
+all_notes = ('A','Bb')
+octaves_chords = (('0','1'),('1','2'))
+
+for octaves in octaves_chords:
+    for note in all_notes:
+        create_note_instances(note, octaves)
+        base_note = f'{note}{octaves[0]}'
+        create_chord_instance(base_note,'major','natural',octaves)
+        
+
+
+
+
+
+
+
 
 # tenho que automatizar isso, programador nao fica repetindo codigo
 # não sei exatamente como fazer isso, mas penso em closure com loops
@@ -35,7 +72,7 @@ class Chord:
 
 # if __name__ == '__main__':
 
-#     c_major_chord = Chord([c3_note, g3_note, c4_note, e4_note, g4_note])
+#    
     
 #     chord_sound = c_major_chord.create_chord()
 #     chord_sound.export("data/chords/chord_c_major_natural.wav", format="wav")
@@ -46,8 +83,6 @@ class Chord:
 
 
 
-for index, notes in enumerate(all_notes,0):
-    print(notes, all_notes[index+4], all_notes[index+4+3])
 
 # pretendo pensar em uma lógica para criar todos os acordes de uma vez
 # tem um padrão ('A','Bb','B','C','Db','D','Eb','E','F','Gb','G','Ab')
